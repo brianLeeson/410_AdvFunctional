38359f987499d597b469713445269569904db6c5
brian leeson <brianeleeson@gmail.com>
2018-05-29 09:52:42 -0700

    embarassingly long time fixing dependencies

--- A4_ConnectFour/src/Lib.hs
+++ A4_ConnectFour/src/Lib.hs
@@ -1,10 +1,14 @@
 module Lib where
 
+import qualified Data.Map as Map
 
+-- Pieces - either X or O
+data Piece = X | O
+  deriving (Show)
+-- Columns
+-- Board - list of columns, col major
 
-
-
-
+-- consider representing the board grid as a map from x-y coordinates to the value of that grid location. Data.Map (from containers)
 
 
 
