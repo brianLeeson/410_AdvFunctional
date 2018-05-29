9e61ed61935c09bfcd594c230ce39eb67d919d36
brian leeson <brianeleeson@gmail.com>
2018-05-29 10:16:10 -0700

    typedefs

--- A4_ConnectFour/src/Lib.hs
+++ A4_ConnectFour/src/Lib.hs
@@ -2,13 +2,25 @@ module Lib where
 
 import qualified Data.Map as Map
 
--- Pieces - either X or O
-data Piece = X | O
+-- Pieces - either X or O or Empty space
+data Piece = X | O | Empty
   deriving (Show)
--- Columns
--- Board - list of columns, col major
 
+-- Columns
+type Column = [Piece]
+
+-- Board - 6 rows 7 cols
 -- consider representing the board grid as a map from x-y coordinates to the value of that grid location. Data.Map (from containers)
+-- https://hackage.haskell.org/package/containers-0.4.0.0/docs/Data-Map.html
+type Board = [Column]
+
+-- function takes a tuple (a,b), a Board and returns the value at Board[a][b]
+
+
+
+
+
+
 
 
 
