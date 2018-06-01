fd9fc89a090a9c524ff15c11656ed66f44e9d38d
brian leeson <brianeleeson@gmail.com>
2018-06-01 08:43:58 -0700

    basic control flow draft. compiles

--- A4_ConnectFour/app/Main.hs
+++ A4_ConnectFour/app/Main.hs
@@ -1,13 +1,40 @@
 module Main where
 
 import Lib
-import qualified Data.Map as Map 
+import qualified Data.Map as Map
+import Data.Char
 
 main :: IO ()
 main = do
-    let x = Map.insert 5 'x' (Map.fromList [(5,'a'), (3,'b')])
-    putStrLn "Hello, world!"
+    putStrLn "Welcome ConnectFour!"
+    forever (playGame)
     
+playGame :: IO ()
+playGame = do
+
+  --secret <- randomRIO range
+  --let (low, high) = range
+  numPlayers <- prompt "How many human players? (1 or 2)"
+  if numPlayers == "1" then putStrLn ("Player1 vs. Compueter!") else putStrLn ("Player1 vs. Player2! ")
+  repeatUntilTrue (playround 5)
+  return ()
+
+-- Have the user make a guess at the secret number. Returns True if the guess
+-- was correct, False otherwise.
+playround :: Int -> IO Bool
+playround secret = do
+  answer <- prompt "What's my number?"
+  let guess = read answer :: Int
+      sensible = all isDigit answer
+  case (sensible, compare guess secret) of
+    (False, _) -> do putStrLn "That's not even a number!"
+                     return False
+    (True, LT) -> do putStrLn "That's too low."
+                     return False
+    (True, GT) -> do putStrLn "That's too high."
+                     return False
+    (True, EQ) -> do putStrLn "That's it!"
+                     return True
 
     
 
--- A4_ConnectFour/src/Lib.hs
+++ A4_ConnectFour/src/Lib.hs
@@ -2,8 +2,6 @@ module Lib where
 
 import qualified Data.Map as Map 
 
-
-
 -- Pieces - either X or O or Empty space
 data Piece = X | O | Empty
   
@@ -23,8 +21,15 @@ type Board = Map.Map Int Column
 emptyColumn = Map.fromList([(0,Empty),(1,Empty),(2,Empty),(3,Empty),(4,Empty),(5,Empty),(6,Empty)])
 emptyBoard = Map.fromList([(0, emptyColumn),(1, emptyColumn),(2, emptyColumn),(3, emptyColumn),(4, emptyColumn),(5, emptyColumn),(6, emptyColumn)])
 
+-- function that takes a board a prints it.
+displayBoard :: Board -> IO()
+displayBoard = undefined
+
+-- function that takes a column and prints it
+displayColumn :: Board -> IO()
+displayColumn = undefined
+
 -- function takes a tuple (a,b), a Board and returns the Piece at Board[a][b], [0][0] is top left
--- TODO add error checking?
 boardPeek :: (Int, Int) -> Board -> Maybe Piece
 boardPeek (row, col) board = do 
     colMap <- Map.lookup col board
@@ -36,11 +41,53 @@ boardInsert (row, col) piece board = do
     oldCol <- Map.lookup col board    
     Just (Map.insert col (Map.insert row piece oldCol) board)
 
--- function that takes a column and a piece and places that piece in that column. 
+-- function that takes a column index, piece, board and places that piece in that column. 
 -- evaluates to nothing if that column doesn't exist or is full
 -- evaluates to Just  an updated board otherwise
+playPiece :: Int -> Piece -> Board -> Board
+playPiece col piece board = undefined
 
 
 
+-- Game Control, based on lecture 3
+theLine :: IO String
+theLine = getLine
+
+-- To chain together several actions, use a 'do' expression.
+getTwoLines :: IO (String, String)
+getTwoLines = do first <- getLine
+                 second <- getLine
+                 return (first, second)
+
+putTwoLines :: String -> String -> IO ()
+putTwoLines first second = do
+  putStrLn first
+  putStrLn second
+
+-- Prompt a user for input by first displaying a question and then waiting for
+-- their answer.
+prompt :: String -> IO String
+prompt question = do putStrLn question
+                     getLine
+
+-- Greet the user by first asking for their name, and then saying hello.
+greet :: IO ()
+greet = do
+  name <- prompt "Who is this?"
+  let greeting = "Hello, " ++ name ++ "!"
+  putStrLn greeting
+
+-- For example, 'forever' runs the given IO action forever.
+forever :: IO a -> IO b
+forever action = do action
+                    forever action
+
+-- run the given action until it returns True.
+repeatUntilTrue :: IO Bool -> IO ()
+repeatUntilTrue action = do
+  x <- action
+  if x
+    then return ()
+    else repeatUntilTrue action
 
 
